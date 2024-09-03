7a7768b409c1a17b7a49ad9207933138db7590fa
Unuching marma <unuching.marma2023@gmail.com>
2024-09-03 21:36:04 +0600

    showing the hidden para

---
+++ script.js
@@ -0,0 +1,11 @@
+const myPara = document.getElementById("para");
+const myButton = document.getElementById("btn");
+
+// function showpara() {
+//   myButton.addEventListener("click", () => {
+//     myPara.style.visibility = "visible";
+//   });
+// }
+function showpara() {
+  myPara.style.visibility = "visible";
+}
